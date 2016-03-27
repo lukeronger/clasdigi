ecaae6fb6c419b02bab33ccebef119545cfa56ac
Whitney Armstrong <warmstrong@anl.gov>
2016-03-27 17:21:00 -0500

    modified:   src/Geo/include/RCGeometry.h 	modified:   src/Geo/src/RCGeometry.cxx

--- src/Geo/include/RCGeometry.h
+++ src/Geo/include/RCGeometry.h
@@ -22,13 +22,26 @@ namespace clas12 {
           Numbering conventions:
             - Each "Layer" has NCells number of unit cells
             - Each  unit cell has 1 sense wire and 8 field wires
-            - Each  unit cell has 4 sub cells
-                     _______ 
+            - Each  unit cell has 4 sub-cells
+              Sub-cell numbering:
+                    2_______3 
                      \__|__/
                       \_|_/
+                      1   0 
             - These sub cells are given the same copy number in geant4
+              Field wire number for cell:
+                    4___5___6 
+                     \__|__/
+                    3 \_|_/ 7
+                      2 1 0 
             - The Field wires surrounding the wire are number going around the wire starting with the inner radius at smallest phi values 
             - Each unit cell has a unique "channel number" starting at 0.
+            - Each sub-cell trapeziod has 8 points defining it
+              upstream:  2_____3     downstream:  6_____7
+                          \   /                    \   /
+                           \_/                      \_/
+                           1 0                      5 4 
+                  
 
        */
       class RCGeometry {
@@ -41,6 +54,10 @@ namespace clas12 {
             int    NCellLayers    = 3;
             double MaxWireSep     = 2.0*mm; // *desired* wire separation around the circumference. 
             double WireLength     = Length/Cos(10.0*degree);
+            double StereoAngle    = 10.0*degree;
+
+            double InnerGap       = 1.0*mm;
+            double LayerGap       = 1.4*mm;
 
             std::array<double, 8>   fCellInnerRadius;
             std::array<int   , 8>   fNCells;
@@ -52,42 +69,19 @@ namespace clas12 {
             RCGeometry();
             void Print();
 
-            int        GetWireLayer(int channel) const {
-               int res = 0;
-               for(auto n: fNCells) {
-                  if( channel >= n) break;
-                  res++;
-               }  
-               return res;
-            }
+            int        GetWireLayer(int channel) const ;
 
-            double GetSenseWireRadius(int channel) const {
-               int lay = GetWireLayer(channel);
-               return fCellCentralRadius.at(lay);
-            }
+            double GetSenseWireRadius(int channel) const ;
 
-            double GetSenseWirePhi(int channel) const {
-               int     lay = GetWireLayer(channel);
-               int     wire_number = channel - fNCells.at(lay);
-               return wire_number*fCellDeltaPhi.at(lay);
-            }
+            double GetSenseWirePhi(int channel) const ;
 
-            Hep3Vector GetSenseWirePosition(int channel) const {
-               int     lay = GetWireLayer(channel);
-               double  r   = GetSenseWireRadius(channel);
-               Hep3Vector res = {0,r,0};
-               res.rotateZ(GetSenseWirePhi(channel));
-               return res;
-            }
+            Hep3Vector GetSenseWirePosition(int channel) const ;
 
-            Hep3Vector GetUpstreamSenseWirePosition(int channel) const {
-               int     lay = GetWireLayer(channel);
-               double  r   = GetSenseWireRadius(channel);
-               Hep3Vector res = {0,r,-WireLength/2.0};
-               res.rotateX(fCellStereoAngle.at(lay));
-               res.rotateZ(GetSenseWirePhi(channel));
-               return res;
-            }
+            Hep3Vector GetUpstreamSenseWirePosition(int channel) const ;
+
+            Hep3Vector GetUnitSubCellPoint(int layer,int subcell, int point) const ;
+
+            std::vector<Hep2Vector> GetSubCellTrapPoints(int layer, int subcell) ;
 
       };
 
--- src/Geo/src/RCGeometry.cxx
+++ src/Geo/src/RCGeometry.cxx
@@ -17,13 +17,13 @@ namespace clas12 {
          // g fsf g ... g fsf g 
          for(int i = 0; i< NLayers; i++){
             // The inner radius for a given layer of wires
-            fCellInnerRadius[i] = (InnerRadius + DeltaR + double(NCellLayers*i)*DeltaR);
+            fCellInnerRadius[i] = (InnerRadius + InnerGap + double(i)*(2.0*DeltaR + LayerGap ));
             fNCells[i]          = 2.0*int(CLHEP::pi*fCellInnerRadius[i]/MaxWireSep);
             fCellDeltaPhi[i]    = (2.*CLHEP::pi/double(fNCells[i]));
             double sign = 1.0;
             if(i%2==0) sign=1.;
             else sign=-1.;
-            fCellStereoAngle[i] = 10.0*sign*CLHEP::degree;
+            fCellStereoAngle[i] = sign*StereoAngle;
             fCellCentralRadius[i] = fCellInnerRadius[i] + DeltaR;
          }
 
@@ -43,7 +43,92 @@ namespace clas12 {
       }
       //______________________________________________________________________________
 
+      int  RCGeometry::GetWireLayer(int channel) const {
+         int res = 0;
+         for(auto n: fNCells) {
+            if( channel >= n) break;
+            res++;
+         }  
+         return res;
+      }
+      //______________________________________________________________________________
+
+
+      double RCGeometry::GetSenseWireRadius(int channel) const {
+         int lay = GetWireLayer(channel);
+         return fCellCentralRadius.at(lay);
+      }
+      //______________________________________________________________________________
+
+
+      double RCGeometry::GetSenseWirePhi(int channel) const {
+         int     lay = GetWireLayer(channel);
+         int     wire_number = channel - fNCells.at(lay);
+         return wire_number*fCellDeltaPhi.at(lay);
+      }
+      //______________________________________________________________________________
+
+
+      Hep3Vector RCGeometry::GetSenseWirePosition(int channel) const {
+         int     lay = GetWireLayer(channel);
+         double  r   = GetSenseWireRadius(channel);
+         Hep3Vector res = {0,r,0};
+         res.rotateZ(GetSenseWirePhi(channel));
+         return res;
+      }
+      //______________________________________________________________________________
+
+
+      Hep3Vector RCGeometry::GetUpstreamSenseWirePosition(int channel) const {
+         int     lay = GetWireLayer(channel);
+         double  r   = GetSenseWireRadius(channel);
+         Hep3Vector res = {0,r,-WireLength/2.0};
+         res.rotateX(fCellStereoAngle.at(lay));
+         res.rotateZ(GetSenseWirePhi(channel));
+         return res;
+      }
+      //______________________________________________________________________________
+
+
+      Hep3Vector RCGeometry::GetUnitSubCellPoint(int layer,int subcell, int point) const {
+         double  r   = fCellInnerRadius.at(layer);
+         double phi_sign = -1.0;
+         double Lsign = -1.0;
+         int    p    = point%4;
+         int    up   = point/4;
+
+         if( (p == 1) || (p == 2) ) {
+            phi_sign  = 1.0;
+         }
+         if( p > 1 ) {
+            r += DeltaR;
+         }
+         if( subcell > 1) {
+            r += DeltaR;
+         }
+         if( up > 0 ) {
+            Lsign = 1.0;
+         }
+         Hep3Vector res2 = {0,0,Lsign*WireLength/2.0};
+         res2.rotateX( fCellStereoAngle.at(layer)   );
+         Hep3Vector res = {r,0,0};
+         res += res2;
+         res.rotateZ( phi_sign*fCellDeltaPhi.at(layer)/2.0 );
+         return( res );
+      }
+      //______________________________________________________________________________
+
+
+      std::vector<Hep2Vector> RCGeometry::GetSubCellTrapPoints(int layer, int subcell) {
+         std::vector<Hep2Vector>  res;
+         for(int i = 0; i <8; i++){
+            Hep3Vector p = GetUnitSubCellPoint(layer, subcell, i);
+            res.push_back( {p.x(),p.y()} );
+         }
+         return res;
+      }
+      //______________________________________________________________________________
+
+
    }
 }
-
-
